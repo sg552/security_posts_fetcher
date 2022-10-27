0ccdcfe64439ac387371f28cf899338b0e8ee89c
申思维 <shensiwei@sina.com>
2022-10-27 09:51:21 +0800

    创建了blog表

--- db/migrate/20221027013150_create_blogs.rb
+++ db/migrate/20221027013150_create_blogs.rb
@@ -1,8 +1,12 @@
 class CreateBlogs < ActiveRecord::Migration[7.0]
   def change
     create_table :blogs do |t|
+      t.string :title
+      t.text :content
+      t.string :author
+      t.string :image_url
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
