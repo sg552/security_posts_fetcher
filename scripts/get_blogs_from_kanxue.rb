b4b1f6ba6064ceead7760a0d1f95c012978817fb
申思维 <shensiwei@sina.com>
2022-10-28 15:54:33 +0800

    完善了抓取阿里雲先知社區的腳本，增加了抓取看雪社區的腳本，尚未完善

---
+++ scripts/get_blogs_from_kanxue.rb
@@ -0,0 +1,57 @@
+ENV['RAILS_ENV'] = ARGV.first || ENV['RAILS_ENV'] || 'development'
+require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
+require 'rails'
+require 'json'
+require 'rubygems'
+require 'httparty'
+require 'nokogiri'
+
+url = "https://www.kanxue.com/"
+kanxue_url = "www.kanxue.com"
+post_html = "/homepost-morearticle.htm"
+Rails.logger.info "===#{url}"
+headers = {
+  'Host': 'www.kanxue.com',
+  'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+  'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Referer': 'https://ctf.pediy.com/',
+  'Connection': 'keep-alive',
+  'Cookie': 'PHPSESSID=8a82f36e5666eb8fd519769699f9ff2e; __jsluid_s=61f26b9d4fbbe7b03612225e7a61b899; Hm_lvt_820e73ad7ccba42be0e5b528c537e327=1666926727; Hm_lpvt_820e73ad7ccba42be0e5b528c537e327=1666926742',
+  'Upgrade-Insecure-Requests': '1',
+  'Sec-Fetch-Dest': 'document',
+  'Sec-Fetch-Mode': 'navigate',
+  'Sec-Fetch-Site': 'cross-site',
+  'Sec-Fetch-User': '?1'
+}
+
+response = HTTParty.get url, :headers => headers
+puts "===response.code, #{response.code} ===response.headers is #{response.headers}"
+doc = Nokogiri::HTML(response.body)
+puts "=== doc is #{doc}"
+doc.css('div[class="media-body position-relative"] a').each do |title|
+  puts "=== title is #{title}"
+  temp_blog_url = title["href"]
+  temp_blog_title = title["title"]
+  puts "== temp_blog_title is #{temp_blog_title}"
+  puts "== temp_blog_url is #{temp_blog_url}"
+  blog_url = ''
+  if temp_blog_url.include? 'https:' || 'http'
+    puts blog_title
+    puts "== include= blog_url is #{blog_url}"
+  else
+    blog_url = "https:#{temp_blog_url}"
+    puts "=else= blog_url is #{blog_url}"
+  end
+
+  blog = Blog.where('blog_url = ?', blog_url).first
+  if blog.blank?
+    if temp_blog_title.present?
+      Blog.create title: temp_blog_title.strip, blog_url: blog_url
+    else
+      Blog.create blog_url: blog_url
+    end
+  end
+end
+puts "== blog.all.size #{Blog.all.size}"
---
+++ scripts/get_the_details_of_a_blog_from_kanxue.rb
@@ -0,0 +1,72 @@
+ENV['RAILS_ENV'] = ARGV.first || ENV['RAILS_ENV'] || 'development'
+require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
+require 'rails'
+require 'open-uri'
+require 'json'
+require 'rubygems'
+require 'httparty'
+require 'nokogiri'
+
+blogs = Blog.all
+blogs.each do |blog|
+  if blog.content.blank?
+    puts "== blog.inspect #{blog.inspect}"
+    kanxue_url = "https://www.kanxue.com/"
+    puts "=== blog.blog_url : #{blog.blog_url}"
+    headers = {
+      'Host': 'bbs.pediy.com',
+      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+      'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Connection': 'keep-alive',
+      'Cookie': 'bbs_sid=6fe6d893ccc97dd453efb5c77932f571; __jsluid_s=33efc98ca86b7d6d182f48c94a5f8824; __utma=181774708.684683680.1666926694.1666926694.1666938778.2; __utmc=181774708; __utmz=181774708.1666926694.1.1.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; _ga=GA1.2.1524884775.1666926736; _gid=GA1.2.1089669572.1666926736; __jsluid_h=213acb53f4c527a280d8e4207e0ba8e4; __utmb=181774708.3.10.1666938778; __utmt=1',
+      'Upgrade-Insecure-Requests': '1',
+      'Sec-Fetch-Dest': 'document',
+      'Sec-Fetch-Mode': 'navigate',
+      'Sec-Fetch-Site': 'none',
+      'Sec-Fetch-User': '?1'
+    }
+    if blog.blog_url.include? 'https:http'
+      new_blog_url = blog.blog_url.sub("https:", '')
+      blog.update blog_url: new_blog_url
+      puts "=== blog: #{blog.inspect}"
+    end
+
+    response = HTTParty.get blog.blog_url, :headers => headers
+    puts "===response.code, #{response.code} === response.headers is #{response.headers}"
+    doc = Nokogiri::HTML(response.body)
+    puts "=== doc is #{doc} doc.class#{doc.class}"
+
+    to_get_titile = doc.css('h3[class="break-all subject m-0"]').text.strip rescue ''
+    puts "=== to_get_titile is #{to_get_titile}"
+
+    to_get_content = doc.css("div[class='message ']") rescue ''
+    puts "==  to_get_content is #{to_get_content}"
+    images = doc.css("div[class='message '] img") rescue ''
+    puts "=== images is #{images}"
+    blog_content = ''
+    if images != ''
+      images.to_ary.each do |image|
+        puts "=== image is #{image}"
+        image_src = image.attr("src") rescue ''
+        puts "--- image_src is #{image_src} "
+        image_name = image_src.sub('upload/attach/202210/', '') rescue ''
+        puts "=== image_src_sub is #{image_name}"
+        `wget -cO - "https://bbs.pediy.com/#{image_src}" > "public/blog_images/#{image_name}"`
+        puts "=== blog_content is #{blog_content}"
+      end
+    end
+    blog_content = to_get_content.to_s.gsub("/upload/attach/202210/", "###MY_IMAGE_SITE###/images/")
+    puts "=== blog_content is #{blog_content}"
+
+    username = doc.css('a[class="btn bg-white w-100 py-1"]') rescue ''
+    to_get_author = username[0]["href"] rescue ''
+    author = to_get_author.split('=').last
+    puts "=== username is #{username} author: #{author}"
+    blog.update author: author, content: blog_content
+    puts '===start 30'
+    sleep 30
+    puts '==end 30'
+  end
+end
