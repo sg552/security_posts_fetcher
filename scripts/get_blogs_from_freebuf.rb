599681770be8a241cf159ff4ac809338e81a59e5
申思维 <shensiwei@sina.com>
2022-11-02 11:41:13 +0800

    完善了安全脉搏详情页面分类的关联关系，freebuf的列表页面的分类修改为专栏

--- scripts/get_blogs_from_freebuf.rb
+++ scripts/get_blogs_from_freebuf.rb
@@ -7,39 +7,37 @@ require 'httparty'
 require 'nokogiri'
 
 i = 1
-
 loop do
-  @logger = Logger.new("#{Rails.root}/log/get_blogs_from_anquanmaibo.log")
-  response = HTTParty.get "https://www.freebuf.com/fapi/frontend/home/article?page=#{i}&limit=20&type=1&day=7&category=%E7%B2%BE%E9%80%89"
-  #response = HTTParty.get "https://www.freebuf.com/fapi/frontend/home/article?page=#{i}"
+  freebuf_url = "https://www.freebuf.com/fapi/frontend/home/article?page=#{i}&limit=20&type=1&day=7&category=%E7%B2%BE%E9%80%89"
+  @logger = Logger.new("#{Rails.root}/log/get_blogs_from_freebuf.log")
+  response = HTTParty.get freebuf_url
   @logger.info response.body
   body = JSON.parse(response.body)
   temp_blogs = body["data"]["list"]
   @logger.info temp_blogs
   temp_blogs.each do |temp_blog|
-    #https://www.freebuf.com/articles/348356.html
-    puts '===='
     url = "https://www.freebuf.com/articles/#{temp_blog["ID"]}.html"
     puts url
     blog_local = Blog.where('blog_url = ?', url).first
     if blog_local.blank?
       title = temp_blog['post_title']
       created_at = temp_blog['post_date']
-      #https://www.freebuf.com/author/%E6%96%97%E8%B1%A1%E7%A7%91%E6%8A%80%E5%AE%98%E6%96%B9
       author_url = "https://www.freebuf.com/author/#{temp_blog['post_author']}"
       remote_image_url = temp_blog['column_post_picture']
       image_name = remote_image_url.gsub('/', '_').gsub(':', '')
       `wget -cO - "#{remote_image_url}" > "public/blog_images/#{image_name}"`
       image_url = "###MY_IMAGE_SITE###/images/#{image_name}"
-      category = temp_blog['category']
+      special_column_name = temp_blog['category']
       author = temp_blog['username']
       views = temp_blog['read_count'].to_i
       @logger.info "=== url: #{url} title: #{title} created_at: #{created_at} author_url: #{author_url} image_url: #{image_url} category: #{category} author:#{author} views:#{views}"
       blog = Blog.create blog_url: url, author: author, views: views, title: title, created_at: created_at, image_url: image_url, source_website: 'freebuf', author_url: author_url
-      category_local = Category.where('name = ? and blog_id = ?', category, blog.id).first
-      if category_local.blank?
-        Category.create name: category, blog_id: blog.id
+      special_column_local = SpecialColumn.where('name = ? and source_website = ?', special_column_name, 'freebuf').first
+      if special_column_local.blank?
+        special_column_local = SpecialColumn.create name: special_column_name, source_website: 'freebuf'
+        @logger.info "===special_column_name:#{special_column_name} special_column_local: #{special_column_local.inspect}"
       end
+      @logger.info "=== SpecialColumn.all.size #{SpecialColumn.all.size}"
     end
   end
   i = i + 1
--- scripts/get_the_details_of_a_blog_from_anquanmaibo.rb
+++ scripts/get_the_details_of_a_blog_from_anquanmaibo.rb
@@ -8,73 +8,81 @@ require 'httparty'
 require 'nokogiri'
 
 @logger = Logger.new("#{Rails.root}/log/get_the_details_of_a_blog_from_anquanmaibo.log")
-blogs = Blog.all
+blogs = Blog.where('source_website = ?', 'anquanmaibo').all
 blogs.each do |blog|
   @logger.info "=== before update blog: #{blog.inspect}"
-  if blog.views.blank? && blog.blog_url.include?('secpulse')
-    @logger.info "== blog.inspect #{blog.inspect}"
-    headers = {
-      'Host': 'www.secpulse.com',
-      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0',
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-      'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'Connection': 'keep-alive',
-      'Cookie': 'Hm_lvt_7f4cc5524dcb1aec487b4266c18bae48=1666943504; Hm_lpvt_7f4cc5524dcb1aec487b4266c18bae48=1666948647',
-      'Upgrade-Insecure-Requests': '1',
-      'Sec-Fetch-Dest': 'document',
-      'Sec-Fetch-Mode': 'navigate',
-      'Sec-Fetch-Site': 'cross-site'
-    }
+  headers = {
+    'Host': 'www.secpulse.com',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Cookie': 'Hm_lvt_7f4cc5524dcb1aec487b4266c18bae48=1666943504; Hm_lpvt_7f4cc5524dcb1aec487b4266c18bae48=1666948647',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'cross-site'
+  }
 
-    response = HTTParty.get blog.blog_url, :headers => headers
-    @logger.info "===response.code, #{response.code} === response.headers is #{response.headers}"
-    doc = Nokogiri::HTML(response.body)
-    @logger.info "=== doc is #{doc} doc.class#{doc.class}"
+  response = HTTParty.get blog.blog_url, :headers => headers
+  @logger.info "===response.code, #{response.code} === response.headers is #{response.headers}"
+  doc = Nokogiri::HTML(response.body)
+  @logger.info "=== doc is #{doc} doc.class#{doc.class}"
 
-    to_get_titile = doc.css('h1').text rescue ''
-    @logger.info "==== to_get_titile: #{to_get_titile}"
-    to_get_author = doc.css('span[class="writer"] a')
-    author_url = "#{to_get_author[0]["href"]}"
-    #获得作者
-    author = "#{to_get_author[0].text}"
-    @logger.info "==== to_get_author : #{to_get_author}==== author_url: #{author_url}==== author: #{author}"
-    created_at = doc.css('div[class="right fr"] span')[0].text
-    views = doc.css('div[class="right fr"] span')[1].text.sub(',', '').to_i
-    @logger.info "==== created_at: #{created_at} views: #{views}"
+  to_get_titile = doc.css('h1').text rescue ''
+  @logger.info "==== to_get_titile: #{to_get_titile}"
+  to_get_author = doc.css('span[class="writer"] a')
+  author_url = "#{to_get_author[0]["href"]}"
+  #获得作者
+  author = "#{to_get_author[0].text}"
+  @logger.info "==== to_get_author : #{to_get_author}==== author_url: #{author_url}==== author: #{author}"
+  created_at = doc.css('div[class="right fr"] span')[0].text
+  views = doc.css('div[class="right fr"] span')[1].text.sub(',', '').to_i
+  @logger.info "==== created_at: #{created_at} views: #{views}"
 
-    to_get_content = doc.css('div[class="left-9-code"]') rescue ''
-    @logger.info "==  to_get_content is #{to_get_content}"
-    images = to_get_content.css('img') rescue ''
-    @logger.info "=== images is #{images}"
-    blog_content = ''
-    remote_uploads_url = "https://secpulseoss.oss-cn-shanghai.aliyuncs.com/wp-content/uploads/"
-    if images != ''
-      images.to_ary.each do |image|
-        @logger.info "=== image is #{image}"
-        image_src = image.attr("src") rescue ''
-        @logger.info "--- image_src is #{image_src} "
-        temp_image_name = image_src.sub("#{remote_uploads_url}", '') rescue ''
-        image_name = temp_image_name.gsub('/', '_')
-        @logger.info "=== image_src_sub is #{image_name}"
-        `wget -cO - "#{image_src}" > "public/blog_images/#{image_name}"`
-      end
+  to_get_content = doc.css('div[class="left-9-code"]') rescue ''
+  @logger.info "==  to_get_content is #{to_get_content}"
+  images = to_get_content.css('img') rescue ''
+  @logger.info "=== images is #{images}"
+  image_remote_and_local_hash = {}
+  if images != ''
+    images.to_ary.each do |image|
+      image_src = image.attr("src") rescue ''
+      temp_image_name = image_src.sub("#{remote_uploads_url}", '') rescue ''
+      image_name = image_src.to_s.gsub('/', '_').gsub(':', '')
+      image_local = "###MY_IMAGE_SITE###/images/#{image_name}"
+      image_remote_and_local_hash.store(image_src, image_local)
+      `wget -cO - "#{image_src}" > "public/blog_images/#{image_name}"`
     end
-    blog_content = to_get_content.to_s.gsub("#{remote_uploads_url}", "###MY_IMAGE_SITE###/images/")
-    @logger.info "=== blog_content is #{blog_content}"
-
-    category_name = doc.css('div[class="left fl"] span')[0].css('a').text
-    category_local = Category.where('blog_id = ? and name = ?', blog.id, category_name).first
-    if category_local.blank?
-      Category.create blog_id: blog.id, name: category_name
-    end
-    @logger.info "=== category_name is #{category_name}"
-    puts "=== category_name is #{category_name}"
-
-    blog.update author: author, author_url: author_url, content: blog_content, views: views, created_at: created_at
-    @logger.info "=== after update blog: #{blog.inspect}"
-    @logger.info '===start 30'
-    sleep 30
-    @logger.info '==end 30'
   end
+  @logger.info "image_remote_and_local_hash #{image_remote_and_local_hash}"
+  blog_content = to_get_content.to_s
+  @logger.info "=== before replace image_url blog_content is #{blog_content}"
+  image_remote_and_local_hash.map {|key, value|
+    if key.to_s.include?('http')
+      @logger.info "==== key #{key} value: #{value}"
+      blog_content = blog_content.to_s.gsub("#{key.to_s}", "#{value.to_s}")
+    end
+    @logger.info "=== after replace image_url blog_content is #{blog_content}"
+  }
+  @logger.info "=== end map content is #{blog_content}"
+
+  special_column_name = doc.css('span[class="tag mr20"] a').text
+  @logger.info "special_column_name #{special_column_name}"
+  special_column_local = SpecialColumn.where('name = ? and source_website = ?', special_column_name, 'anquanmaibo').first
+  if special_column_local.blank?
+    special_column_local = SpecialColumn.create name: special_column_name, source_website: 'anquanmaibo'
+  end
+  doc.css('span[class="tags"] a').each do |a|
+    category_name = a.text
+    @logger.info "=== category_name #{category_name}"
+    Category.create name: category_name, blog_id: blog.id, special_column_id: special_column_local.id
+  end
+
+  blog.update author: author, author_url: author_url, content: blog_content, views: views, created_at: created_at
+  @logger.info "=== after update blog: #{blog.inspect}"
+  @logger.info '===start 30'
+  sleep 30
+  @logger.info '==end 30'
 end
