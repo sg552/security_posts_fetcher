48d6369d4080a2de8ad95581f092f7f2bf07b0e9
申思维 <shensiwei@sina.com>
2022-12-30 16:57:31 +0800

    增加了根据网站来源进行抓取看雪博客详情,修改了抓取先知博客的脚本，如果抓到的博客内容为空重新抓取三次

--- scripts/get_the_details_of_a_blog_from_kanxue.rb
+++ scripts/get_the_details_of_a_blog_from_kanxue.rb
@@ -8,83 +8,93 @@ require 'httparty'
 require 'nokogiri'
 
 @logger = Logger.new("#{Rails.root}/log/get_the_details_of_a_blog_from_kanxue.log")
-blogs = Blog.all
-blogs.each do |blog|
-  if blog.content.blank?
-    @logger.info "== blog.inspect #{blog.inspect}"
-    kanxue_url = "https://www.kanxue.com/"
-    @logger.info "=== blog.blog_url : #{blog.blog_url}"
-    headers = {
-      'Host': 'bbs.pediy.com',
-      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0',
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-      'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'Connection': 'keep-alive',
-      'Cookie': '__jsluid_s=33efc98ca86b7d6d182f48c94a5f8824; __utma=181774708.684683680.1666926694.1667180033.1667204111.6; __utmz=181774708.1667180033.5.3.utmcsr=kanxue.com|utmccn=(referral)|utmcmd=referral|utmcct=/; _ga=GA1.2.1524884775.1666926736; __jsluid_h=213acb53f4c527a280d8e4207e0ba8e4; __utmc=181774708; bbs_sid=d33a89264e69595dbc6960aff7bc614e; __utmb=181774708.3.10.1667204111; __utmt=1',
-      'Upgrade-Insecure-Requests': '1',
-      'Sec-Fetch-Dest': 'document',
-      'Sec-Fetch-Mode': 'navigate',
-      'Sec-Fetch-Site': 'none',
-      'Sec-Fetch-User': '?1'
-    }
-    if blog.blog_url.include? 'https:http'
-      new_blog_url = blog.blog_url.sub("https:", '')
-      blog.update blog_url: new_blog_url
-      @logger.info "=== blog: #{blog.inspect}"
-    end
+blogs = Blog.where('source_website = ?', 'kanxue')
+if blogs.present?
+  blogs.each do |blog|
+    begin
+      if blog.content.blank?
+        @logger.info "== blog.inspect #{blog.inspect}"
+        kanxue_url = "https://www.kanxue.com/"
+        @logger.info "=== blog.blog_url : #{blog.blog_url}"
+        headers = {
+          'Host': 'bbs.pediy.com',
+          'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0',
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+          'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+          'Accept-Encoding': 'gzip, deflate, br',
+          'Connection': 'keep-alive',
+          'Cookie': '__jsluid_s=33efc98ca86b7d6d182f48c94a5f8824; __utma=181774708.684683680.1666926694.1667180033.1667204111.6; __utmz=181774708.1667180033.5.3.utmcsr=kanxue.com|utmccn=(referral)|utmcmd=referral|utmcct=/; _ga=GA1.2.1524884775.1666926736; __jsluid_h=213acb53f4c527a280d8e4207e0ba8e4; __utmc=181774708; bbs_sid=d33a89264e69595dbc6960aff7bc614e; __utmb=181774708.3.10.1667204111; __utmt=1',
+          'Upgrade-Insecure-Requests': '1',
+          'Sec-Fetch-Dest': 'document',
+          'Sec-Fetch-Mode': 'navigate',
+          'Sec-Fetch-Site': 'none',
+          'Sec-Fetch-User': '?1'
+        }
+        if blog.blog_url.include? 'https:http'
+          new_blog_url = blog.blog_url.sub("https:", '')
+          @logger.info "== new_blog_url #{new_blog_url} blog.blog_url #{blog.blog_url}"
+          blog.update blog_url: new_blog_url
+          @logger.info "=== blog: #{blog.inspect}"
+        elsif blog.blog_url.include? '//'
+          blog.update blog_url: "https:#{blog.blog_url}"
+          @logger.info "=== blog: #{blog.inspect}"
+        end
 
-    response = HTTParty.get blog.blog_url, :headers => headers
-    @logger.info "===response.code, #{response.code} === response.headers is #{response.headers}"
-    doc = Nokogiri::HTML(response.body)
-    @logger.info "=== doc is #{doc} doc.class#{doc.class}"
+        response = HTTParty.get blog.blog_url, :headers => headers
+        #@logger.info "===response.code, #{response.code} === response.headers is #{response.headers}"
+        doc = Nokogiri::HTML(response.body)
+        #@logger.info "=== doc is #{doc} doc.class#{doc.class}"
 
-    title = doc.css('h3').text.strip rescue ''
-    author = doc.css('div[class="col"] span')[0].text.strip rescue ''
-    created_at = doc.css('span[class="date text-grey ml-1"]').text.strip rescue ''
-    #created_at = doc.css('div[class="col"] span')[1].text.strip rescue ''
-    views = doc.css('div[class="col"] span')[2].text.to_i rescue ''
+        title = doc.css('h3').text.strip rescue ''
+        author = doc.css('div[class="col"] span')[0].text.strip rescue ''
+        created_at = doc.css('span[class="date text-grey ml-1"]').text.strip rescue ''
+        #created_at = doc.css('div[class="col"] span')[1].text.strip rescue ''
+        views = doc.css('div[class="col"] span')[2].text.to_i rescue ''
 
-    to_get_content = doc.css('div[isfirst="1"]') rescue ''
-    @logger.info "==  to_get_content is #{to_get_content}"
-    images = doc.css("div[class='message '] img") rescue ''
-    @logger.info "=== images is #{images}"
-    blog_content = ''
-    if images != ''
-      images.to_ary.each do |image|
-        @logger.info "=== image is #{image}"
-        image_src = image.attr("src") rescue ''
-        @logger.info "--- image_src is #{image_src} "
-        image_name = image_src.sub('upload/attach/202210/', '') rescue ''
-        @logger.info "=== image_src_sub is #{image_name}"
-        `wget -cO - "https://bbs.pediy.com/#{image_src}" > "public/blog_images/#{image_name}"`
+        to_get_content = doc.css('div[isfirst="1"]') rescue ''
+        @logger.info "==  to_get_content is #{to_get_content}"
+        images = doc.css("div[class='message '] img") rescue ''
+        @logger.info "=== images is #{images}"
+        blog_content = ''
+        if images != ''
+          images.to_ary.each do |image|
+            @logger.info "=== image is #{image}"
+            image_src = image.attr("src") rescue ''
+            @logger.info "--- image_src is #{image_src} "
+            image_name = image_src.sub('upload/attach/202210/', '') rescue ''
+            @logger.info "=== image_src_sub is #{image_name}"
+            `wget -cO - "https://bbs.pediy.com/#{image_src}" > "public/blog_images/#{image_name}"`
+          end
+        end
+        blog_content = to_get_content.to_s.gsub("/upload/attach/202210/", "###MY_IMAGE_SITE###/images/")
+        @logger.info "=== blog_content is #{blog_content}"
+
+        doc.css('div[class="my-3"] a').each do |a|
+          @logger.info "=== a is #{a}"
+          category_name = a.text
+          @logger.info "=== category_name is #{category_name}"
+          category_local = Category.where('blog_id = ? and name = ?', blog.id, category_name).first
+          @logger.info "=== category_local #{category_local.inspect}"
+          if category_local.blank?
+            Category.create blog_id: blog.id, name: category_name
+          end
+        end
+
+        @logger.info "=== title is #{title} === views: #{views} == created_at:#{created_at} author: #{author}"
+        if created_at.include?('天')
+          temp_created_at =  created_at.split('天').first.to_i
+          @logger.info "=== temp_created_at #{temp_created_at}"
+          created_at = Time.now - temp_created_at * 3600 * 24
+          @logger.info "=== created_at#{created_at}"
+        end
+        blog.update title: title, author: author, content: blog_content, views: views, created_at: created_at, source_website: 'kanxue'
+        @logger.info "=====after update blog: #{blog.inspect}"
+        @logger.info '===start 30'
+        sleep 10
+        @logger.info '==end 30'
       end
+    rescue
+      puts "=== hihi"
     end
-    blog_content = to_get_content.to_s.gsub("/upload/attach/202210/", "###MY_IMAGE_SITE###/images/")
-    @logger.info "=== blog_content is #{blog_content}"
-
-    doc.css('div[class="my-3"] a').each do |a|
-      @logger.info "=== a is #{a}"
-      category_name = a.text
-      @logger.info "=== category_name is #{category_name}"
-      category_local = Category.where('blog_id = ? and name = ?', blog.id, category_name).first
-      @logger.info "=== category_local #{category_local.inspect}"
-      if category_local.blank?
-        Category.create blog_id: blog.id, name: category_name
-      end
-    end
-
-    @logger.info "=== title is #{title} === views: #{views} == created_at:#{created_at} author: #{author}"
-    if created_at.include?('天')
-      temp_created_at =  created_at.split('天').first.to_i
-      @logger.info "=== temp_created_at #{temp_created_at}"
-      created_at = Time.now - temp_created_at * 3600 * 24
-      @logger.info "=== created_at#{created_at}"
-    end
-    blog.update title: title, author: author, content: blog_content, views: views, created_at: created_at, source_website: 'kanxue'
-    @logger.info "=====after update blog: #{blog.inspect}"
-    @logger.info '===start 30'
-    sleep 10
-    @logger.info '==end 30'
   end
 end
--- scripts/get_xianzhi_blogs_using_proxy.rb
+++ scripts/get_xianzhi_blogs_using_proxy.rb
@@ -6,12 +6,12 @@ require 'rubygems'
 require 'httparty'
 require 'nokogiri'
 
-Rails.logger = Logger.new("log/get_xianzhi_blogs_using_proxy.log")
+Rails.logger = Logger.new("log/get_xianzhi_blogs_using_proxy#{ENV['FROM']}_#{ENV['TO']}.log")
 
 SLEEP = 30
 URL = 'https://xz.aliyun.com'
-TIMEOUT = 3
-NUMBER = 10
+TIMEOUT = 10
+NUMBER = 3
 
 def get_proxy_token
   command_get_token = %Q{curl -d "user=bigbanana666&password=bigbanana888" https://dvapi.doveproxy.net/cmapi.php?rq=login}
@@ -24,7 +24,7 @@ end
 def get_ip_and_port
   proxy_token = get_proxy_token
   puts "proxy_token#{proxy_token}"
-  city = ['in', 'id', 'ru', 'bd', 'za'].shuffle.first
+  city = ['in', 'hk', 'tw', 'bd', 'za'].shuffle.first
   command_get_ip = %Q{curl -ipv4 -d "user=bigbanana666&token=#{proxy_token}&geo=#{city}&num=#{NUMBER}&timeout=#{TIMEOUT}" https://dvapi.doveproxy.net/cmapi.php?rq=distribute}
   puts "command_get_ip #{command_get_ip}"
   Rails.logger.info "========== command_get_ip #{command_get_ip}"
@@ -36,16 +36,13 @@ def get_ip_and_port
 end
 
 def create_proxy
-  loop do
-    datas = get_ip_and_port
-    if datas.present?
-      datas.each do |data|
-        Proxy.create ip: data['ip'], port: data['port'], external_ip: data['d_ip'], expiration_time: (Time.now + TIMEOUT * 60) if data.present?
-      end
+  datas = get_ip_and_port
+  if datas.present?
+    datas.each do |data|
+      Proxy.create ip: data['ip'], port: data['port'], external_ip: data['d_ip'], expiration_time: (Time.now + TIMEOUT * 60) if data.present?
     end
-    Proxy.where('expiration_time < ?', Time.now).delete_all
-    sleep TIMEOUT * 60
   end
+  Proxy.where('expiration_time < ?', Time.now).delete_all
 end
 
 def save_images images
@@ -56,9 +53,14 @@ def save_images images
     image_name = image_src.to_s.gsub('/', '_').gsub(':', '')
     image_local = "###MY_IMAGE_SITE###/images/#{image_name}"
     image_remote_and_local_hash.store(image_src, image_local)
-    proxy = Proxy.where('expiration_time > ?', Time.now).all.shuffle.first
+    proxy = use_proxy
     Rails.logger.info "==== in save_images proxy ip: #{proxy.external_ip rescue ''}"
-    command_get_image = %Q{curl -s --socks5 #{proxy.ip}:#{proxy.port} #{image_src} --output #{image_local}}
+    begin
+      command_get_image = %Q{curl -s --socks5 #{proxy.ip}:#{proxy.port} #{image_src} --output public/blog_images/#{image_name}}
+    rescue
+      proxy = use_proxy
+      command_get_image = %Q{curl -s --socks5 #{proxy.ip}:#{proxy.port} #{image_src} --output public/blog_images/#{image_name}}
+    end
     Rails.logger.info "======  command_get_image: #{command_get_image}"
     result = `#{command_get_image}`
     Rails.logger.info "=== result #{result} command_get_image: #{command_get_image}"
@@ -66,32 +68,29 @@ def save_images images
   return image_remote_and_local_hash
 end
 
-def create_category doc, blog_url
-  blog = Blog.where('blog_url = ?', blog_url).first
-  if blog.present?
-    to_get_category = doc.css('span[class="content-node"] a').each do |a|
-      category_name  = a.text
-      Rails.logger.info "======  category_name : #{category_name}"
-      category = Category.where('name = ? and blog_id = ?', category, blog.id).first
-      xianzhi_anquanjishu_column = ["安全技术", "众测渗透", "漏洞分析", "WEB安全", "二进制安全", "移动安全", "IoT安全", "企业安全", "区块链安全", "密码学", "CTF", "安全工具", "资源分享", "技术讨论"].to_s
-      xianzhi_qingbao_column = ["情报", "先知情报"].to_s
-      xianzhi_gonggao_column = ["社区公告"].to_s
-      special_column_local = ''
-      if xianzhi_anquanjishu_column.include? category_name
-        special_column_local = SpecialColumn.where('name = ? and source_website = ?', "安全技术", 'xianzhi').first
-      elsif xianzhi_qingbao_column.include? category_name
-        special_column_local = SpecialColumn.where('name = ? and source_website = ?', "先知情报", 'xianzhi').first
-      elsif xianzhi_gonggao_column.include? category_name
-        special_column_local = SpecialColumn.where('name = ? and source_website = ?', "社区公告", 'xianzhi').first
-      end
-      if category.blank?
-        Category.create name: category_name, blog_id: blog.id, special_column_id: special_column_local.id
-      end
-      Rails.logger.info "======  category: #{category.inspect} to_get_category: #{to_get_category}"
+def create_category blog_id, category_name
+  if category_name.present?
+    xianzhi_anquanjishu_column = ["安全技术", "众测渗透", "漏洞分析", "WEB安全", "二进制安全", "移动安全", "IoT安全", "企业安全", "区块链安全", "密码学", "CTF", "安全工具", "资源分享", "技术讨论"].to_s
+    xianzhi_qingbao_column = ["情报", "先知情报"].to_s
+    xianzhi_gonggao_column = ["社区公告"].to_s
+    if category_name.include?("安全")
+      special_column_local = SpecialColumn.where('name = ? and source_website = ?', "安全技术", 'xianzhi').first
+      Rails.logger.info "==== id : #{special_column_local.id rescue ''}"
+    elsif category_name.include?("情报")
+      special_column_local = SpecialColumn.where('name = ? and source_website = ?', "先知情报", 'xianzhi').first
+      Rails.logger.info "==== id : #{special_column_local.id rescue ''}"
+    elsif category_name.include?("社区")
+      special_column_local = SpecialColumn.where('name = ? and source_website = ?', "社区公告", 'xianzhi').first
+      Rails.logger.info "==== id : #{special_column_local.id rescue ''}"
     end
+    category = Category.where('name = ? and blog_id = ?', category_name, blog_id).first
+    Rails.logger.info "======  category: #{category.inspect}"
+    if category.blank?
+      Category.create name: category_name, blog_id: blog_id, special_column_id: special_column_local.id
+    end
+    Rails.logger.info "======  category_name : #{category_name} special_column_local: #{special_column_local} special_column_local.id : #{special_column_local.id}"
   end
 end
-
 
 def use_proxy
   proxy = Proxy.where('expiration_time > ?', Time.now).all.shuffle.first
@@ -103,6 +102,98 @@ def use_proxy
 end
 
 Rails.logger.info "====env['to'] #{ENV["TO"]} env['from'] #{ENV["FROM"]}======== ENV['URL']#{ENV["URL"]}"
+
+def retry_to_get_blog_show_page blog_url
+  proxy = use_proxy
+  command_get_page = %Q{curl -s --socks5 #{proxy.ip}:#{proxy.port} #{blog_url}}
+  Rails.logger.info "======-------------------==command_get_page: #{command_get_page}"
+  blog_html_doc = `#{command_get_page}`
+  blog_show_doc = Nokogiri::HTML(blog_html_doc)
+  to_get_content = blog_show_doc.css('div#topic_content') rescue ''
+  return blog_show_doc
+end
+
+def retry_three_to_get_blog_show_page blog_url
+  begin
+   blog_show_doc = retry_to_get_blog_show_page blog_url
+  rescue
+    begin
+      blog_show_doc = retry_to_get_blog_show_page blog_url
+    rescue
+      begin
+        blog_html_doc = retry_to_get_blog_show_page blog_url
+     rescue
+       Rails.logger.info "======== get blog_show_doc error"
+     end
+   end
+  end
+end
+
+def update_blog blog_url, proxy_id
+  blog = Blog.where('blog_url = ?', blog_url).first
+  #proxy = use_proxy
+  proxy = Proxy.where('id = ?', proxy_id).first
+  if blog.blog_url.present? && blog.content.blank?
+    begin
+      command_get_page = %Q{curl -s --socks5 #{proxy.ip}:#{proxy.port} #{blog.blog_url}}
+    rescue
+      proxy = use_proxy
+      command_get_page = %Q{curl -s --socks5 #{proxy.ip}:#{proxy.port} #{blog.blog_url}}
+    end
+    Rails.logger.info "======-------------------==command_get_page: #{command_get_page}"
+    blog_html_doc = `#{command_get_page}`
+    blog_show_doc = Nokogiri::HTML(blog_html_doc)
+    to_get_content = blog_show_doc.css('div#topic_content') rescue ''
+    blog_show_doc = retry_three_to_get_blog_show_page if to_get_content.blank?
+    to_get_content = blog_show_doc.css('div#topic_content') rescue ''
+    to_get_author = blog_show_doc.css('span[class="info-left"] a')
+    author_url = "#{URL}#{to_get_author[0]}"
+    Rails.logger.info "==  to_get_content is #{to_get_content}  author_url #{author_url}"
+    # 获得博客内容的所有图片
+    images = blog_show_doc.css('div#topic_content img') rescue ''
+    Rails.logger.info "=== images is #{images}"
+    # 为了保存图片
+    image_remote_and_local_hash = save_images images if images.present?
+    Rails.logger.info "====== image_remote_and_local_hash #{image_remote_and_local_hash}"
+    # 获得博客的内容
+    if to_get_content.present? && image_remote_and_local_hash.present?
+      blog_content = to_get_content.to_s
+      Rails.logger.info "=== before replace image_url blog_content is #{blog_content}"
+      image_remote_and_local_hash.map {|key, value|
+        if key.to_s.include?('http')
+          Rails.logger.info "==== key #{key} value: #{value}"
+          blog_content = blog_content.to_s.gsub("#{key.to_s}", "#{value.to_s}")
+        end
+        Rails.logger.info "=== after replace image_url blog_content is #{blog_content}"
+      }
+      Rails.logger.info "=== end map content is #{blog_content}"
+    elsif to_get_content.present?
+      blog_content = to_get_content.to_s
+    end
+
+    username = blog_show_doc.css('span[class="username cell"]').text rescue ''
+    Rails.logger.info "=== username is #{username}"
+    to_get_created_at = blog_show_doc.css('span[class="info-left"] span')
+    temp_to_get_created_at = blog_show_doc.css('span[class="info-left"] span')[5] rescue ''
+    # 获得浏览数
+    views = to_get_created_at[4].text.split('数').last.to_i rescue ''
+    # 获得创建时间
+    created_at = blog_show_doc.css('span[class="info-left"] span')[2].text rescue ''
+    Rails.logger.info "======  to_get_created_at #{to_get_created_at}  views : #{views}  created_at : #{created_at}"
+    # create blog
+    if created_at.blank?
+      blog.update source_website: 'xianzhi', author: username, content: blog_content, views: views
+    else
+      blog.update source_website: 'xianzhi', author: username, content: blog_content, created_at: created_at, views: views
+    end
+    category_name = blog_show_doc.css('span[class="content-node"] a').text rescue ''
+    Rails.logger.info "========= category_name:#{category_name}"
+    create_category blog.id, category_name
+    sleep SLEEP
+  end
+end
+
+
 def run
   (ENV["FROM"].to_i .. ENV["TO"].to_i).each do |i|
     xianzhi_url = "#{ENV['URL']}?page=#{i}"
@@ -118,70 +209,21 @@ def run
     doc.css('p[class="topic-summary"] a').each do |title|
       blog_url = "#{URL}#{title["href"]}" rescue ''
       blog_title = title.text rescue ''
-      Rails.logger.info "=============title: #{title} blog_title is #{blog_title}"
       blog_local = Blog.where('blog_url = ?', blog_url).first
+      Rails.logger.info "=====blog_url: #{blog_url} blog_local: #{blog_local.inspect}========title: #{title} blog_title is #{blog_title}"
       if blog_url.present? && blog_local.blank?
-        command_get_page = %Q{curl -s --socks5 #{proxy.ip}:#{proxy.port} #{blog_url}}
-        Rails.logger.info "======-------------------==command_get_page: #{command_get_page}"
-        blog_html_doc = `#{command_get_page}`
-        blog_show_doc = Nokogiri::HTML(blog_html_doc)
-        to_get_author = blog_show_doc.css('span[class="info-left"] a')
-        author_url = "#{URL}#{to_get_author[0]}"
-        to_get_content = blog_show_doc.css('div#topic_content') rescue ''
-        Rails.logger.info "==  to_get_content is #{to_get_content}  author_url #{author_url}"
-        # 获得博客内容的所有图片
-        images = blog_show_doc.css('div#topic_content img') rescue ''
-        Rails.logger.info "=== images is #{images}"
-        # 为了保存图片
-        #image_remote_and_local_hash = save_images images if images.present?
-        image_remote_and_local_hash = {}
-        images.to_ary.each do |image|
-          image_src = image.attr("src")
-          # 保存本地图片的名称
-          image_name = image_src.to_s.gsub('/', '_').gsub(':', '')
-          image_local = "###MY_IMAGE_SITE###/images/#{image_name}"
-          image_remote_and_local_hash.store(image_src, image_local)
-          #`wget -cO - "#{image_src}" > "public/blog_images/#{image_name}"`
-          Rails.logger.info "==== in save_images proxy ip: #{proxy.external_ip rescue ''}"
-          command_get_image = %Q{curl -s --socks5 #{proxy.ip}:#{proxy.port} #{image_src} --output #{image_local}}
-          Rails.logger.info "======  command_get_image: #{command_get_image}"
-          result = `#{command_get_image}`
-          Rails.logger.info "=== result #{result} command_get_image: #{command_get_image}"
-        end
-        # 获得博客的内容
-        if to_get_content.present?
-          blog_content = to_get_content.to_s
-          Rails.logger.info "=== before replace image_url blog_content is #{blog_content}"
-          image_remote_and_local_hash.map {|key, value|
-            if key.to_s.include?('http')
-              Rails.logger.info "==== key #{key} value: #{value}"
-              blog_content = blog_content.to_s.gsub("#{key.to_s}", "#{value.to_s}")
-            end
-            Rails.logger.info "=== after replace image_url blog_content is #{blog_content}"
-          }
-          Rails.logger.info "=== end map content is #{blog_content}"
-        end
-
-        username = blog_show_doc.css('span[class="username cell"]').text
-        Rails.logger.info "=== username is #{username}"
-        to_get_created_at = blog_show_doc.css('span[class="info-left"] span')
-        temp_to_get_created_at = blog_show_doc.css('span[class="info-left"] span')[5]
-        # 获得浏览数
-        views = to_get_created_at[4].text.split('数').last.to_i
-        # 获得创建时间
-        created_at = blog_show_doc.css('span[class="info-left"] span')[2].text
-        Rails.logger.info "======  to_get_created_at #{to_get_created_at}  views : #{views}  created_at : #{created_at}"
-        # create blog
-        Blog.create title: blog_title.strip, blog_url: blog_url, source_website: 'xianzhi', author: username, content: blog_content, created_at: created_at, views: views
-        create_category blog_show_doc, blog_url
-        sleep SLEEP
+        Blog.create blog_url: blog_url, title: blog_title.strip, source_website: 'xianzhi'
+        Rails.logger.info "========= after create"
+        update_blog blog_url, proxy.id
+      elsif blog_local.present? && blog_local.content.blank?
+        update_blog blog_url, proxy.id
       end
-      sleep SLEEP
     end
 
     Rails.logger.info "=== i: #{i} sleep #{SLEEP}"
     sleep SLEEP
   end
+  Rails.logger.info "=======#{ENV['FROM']}_#{ENV['TO']}======== end"
 end
 
 
