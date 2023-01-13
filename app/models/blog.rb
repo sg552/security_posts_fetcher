91d84b2b17badda2bc6f07194474bebc1a6a6e9a
申思维 <shensiwei@sina.com>
2023-01-13 01:10:49 +0000

    修改了抓取先知社区文章的log，方便查看

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,14 +3,15 @@ class Blog < ActiveRecord::Base
   after_commit :update_jobs, on: :create
 
   def update_jobs
-    if self.blog_url.present? && self.source_website == 'kanxue'
+    if self.blog_url.present? && self.source_website == 'kanxue' && self.content.blank?
       if self.blog_url.include? 'article'
         UpdateKanxueBlogUsingProxyJob.perform_later blog: self
       else
         UpdateKanxueBlogUsingPlaywrightJob.perform_later blog: self
       end
-    elsif self.blog_url.present? && self.source_website == 'xianzhi'
+    elsif self.blog_url.present? && self.source_website == 'xianzhi' && self.content.blank?
       UpdateXianzhiBlogUsingProxyJob.perform_later blog: self
     end
   end
+
 end
--- scripts/get_xianzhi_blogs_using_proxy.rb
+++ scripts/get_xianzhi_blogs_using_proxy.rb
@@ -20,15 +20,15 @@ def retry_to_get_list_page xianzhi_url
   n = 0
   begin
     proxy = Proxy.where('expiration_time > ?', Time.now).all.shuffle.first
-    Rails.logger.info "==== proxy ip: #{proxy.external_ip rescue ''}"
+    Rails.logger.info "==== step 1.3 retry_to_get_list_page url: #{xianzhi_url} The proxy IP for accessing the Xianzhi community is: #{proxy.external_ip rescue ''}"
     command_get_page = %Q{curl -s --socks5 #{proxy.ip}:#{proxy.port} #{xianzhi_url}}
     result = `#{command_get_page}`
-    Rails.logger.info "=== result #{result} command_get_page: #{command_get_page}"
+    Rails.logger.info "===  step 1.3.1 retry_to_get_list_page url: #{xianzhi_url}The command to get the Xianzhi community list page is: #{command_get_page} The return result is #{result}"
     doc = Nokogiri::HTML(result)
-    Rails.logger.info "============= doc is #{doc} "
+    Rails.logger.info "===== step1.4  url: #{xianzhi_url} The html of the Xianzhi community list page is #{doc} "
     list_page_titles = doc.css('p[class="topic-summary"] a')
   rescue => error
-    Rails.logger.info "=== error #{error}"
+    Rails.logger.error "=== setp1.5 url: #{xianzhi_url} to get the html of the Xianzhi community list page error : #{error}"
     n = n + 1
     retry if n < 3
   end
@@ -39,19 +39,19 @@ def run
   (ENV["FROM"].to_i .. ENV["TO"].to_i).each do |i|
     xianzhi_url = "#{ENV['URL']}?page=#{i}"
     proxy = Proxy.where('expiration_time > ?', Time.now).all.shuffle.first
-    Rails.logger.info "==== proxy ip: #{proxy.external_ip rescue ''}"
+    Rails.logger.info "====  xianzhi_url #{xianzhi_url} step1 proxy ip: #{proxy.external_ip rescue ''}"
     command_get_page = %Q{curl -s --socks5 #{proxy.ip}:#{proxy.port} #{xianzhi_url}}
     result = `#{command_get_page}`
-    Rails.logger.info "=== result #{result} command_get_page: #{command_get_page}"
+    Rails.logger.info "=== xianzhi_url #{xianzhi_url} step1.1  result #{result} command_get_page: #{command_get_page}"
     doc = Nokogiri::HTML(result)
-    Rails.logger.info "============= list_page doc is #{doc}"
+    Rails.logger.info "======xianzhi_url #{xianzhi_url} step1.2  list_page doc is #{doc}"
     list_page_titles = doc.css('p[class="topic-summary"] a') rescue ''
     list_page_titles = retry_to_get_list_page xianzhi_url if list_page_titles.blank?
     doc.css('p[class="topic-summary"] a').each do |title|
       blog_url = "#{URL}#{title["href"]}" rescue ''
       blog_title = title.text rescue ''
       blog_local = Blog.where('blog_url = ?', blog_url).first
-      Rails.logger.info "===== blog_url: #{blog_url} blog_local: #{blog_local.inspect} ========title: #{title} blog_title is #{blog_title}"
+      Rails.logger.info "===== xianzhi_url #{xianzhi_url} step2  blog_url: #{blog_url} blog_local: #{blog_local.inspect} ========title: #{title} blog_title is #{blog_title}"
       if blog_url.present? && blog_local.blank?
         Blog.create blog_url: blog_url, title: blog_title.strip, source_website: 'xianzhi'
         sleep SLEEP
